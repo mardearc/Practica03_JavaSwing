b0cf5d8d66b2fdfd91a6592391b6215e502885b5
mardearc <jesumar01@gmail.com>
2024-11-15 20:59:25 +0100

    FramePrincipal

--- Practica03/src/view/FramePrincipal.java
+++ Practica03/src/view/FramePrincipal.java
@@ -5,15 +5,21 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import controller.CtrlCuenta;
+
 import java.awt.BorderLayout;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class FramePrincipal extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JButton btnCargar, btnGuardar, btnVaciar;
 
 	/**
 	 * Launch the application.
@@ -34,7 +40,17 @@ public class FramePrincipal extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public FramePrincipal() {
+	public FramePrincipal(){
+
+		// LLAMADA AL MÉTODO PARA INICIALIZAR LOS COMPONENTES
+		addComponents();
+
+		// LLAMADA AL MÉTODO PARA AÑADIR EVENTOS A LOS COMPONENTES
+		addListeners();
+
+	}
+
+	private void addComponents() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 497, 300);
 		
@@ -56,12 +72,31 @@ public class FramePrincipal extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-		contentPane.setLayout(new BorderLayout(0, 0));
+		contentPane.setLayout(null);
 		
-		JPanel panelInicio = new JPanelInsertar();
-		contentPane.add(panelInicio, BorderLayout.CENTER);
-		panelInicio.setLayout(null);
+		btnCargar = new JButton("Cargar");
 		
+		btnCargar.setBounds(86, 206, 85, 21);
+		contentPane.add(btnCargar);
 		
+		btnGuardar = new JButton("Guardar");
+		btnGuardar.setBounds(196, 206, 85, 21);
+		contentPane.add(btnGuardar);
+		
+		btnVaciar = new JButton("Vaciar");
+		btnVaciar.setBounds(305, 206, 85, 21);
+		contentPane.add(btnVaciar);
+
 	}
+
+	private void addListeners() {
+		btnCargar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				CtrlCuenta ctrlCuenta = new CtrlCuenta();
+				ctrlCuenta.leerFichero();
+			}
+		});
+
+	}
+
 }
