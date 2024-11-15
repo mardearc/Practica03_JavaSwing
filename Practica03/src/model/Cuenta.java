8b8f960d4b1830e5a220ff4635e3464d27d811c5
mardearc <jesumar01@gmail.com>
2024-11-15 18:50:45 +0100

    Clase Cuenta

--- Practica03/src/model/Cuenta.java
+++ Practica03/src/model/Cuenta.java
@@ -1,5 +1,71 @@
 package model;
 
+import java.util.GregorianCalendar;
+
 public class Cuenta {
+
+	private int numero;
+	private transient String titular;
+	private double saldo;
+	private double saldoMinimo;
+	private GregorianCalendar fechaApertura;
+
+	public Cuenta(int numero, String titular, double saldo, double saldoMinimo, GregorianCalendar fechaApertura) {
+		setNumero(numero);
+		setTitular(titular);
+		setSaldo(saldo);
+		setSaldoMinimo(saldoMinimo);
+		setFechaApertura(fechaApertura);
+	}
+
+	public int getNumero() {
+
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		if (numero >= 1 && numero <= 1000) {
+			this.numero = numero;
+		} else {
+			// throwException
+		}
+
+	}
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		if (saldo >= saldoMinimo) {
+			this.saldo = saldo;
+		}else {
+			//throwException
+		}
+	}
+
+	public double getSaldoMinimo() {
+		return saldoMinimo;
+	}
+
+	public void setSaldoMinimo(double saldoMinimo) {
+		this.saldoMinimo = saldoMinimo;
+	}
+
+	public GregorianCalendar getFechaApertura() {
+		return fechaApertura;
+	}
+
+	public void setFechaApertura(GregorianCalendar fechaApertura) {
+		this.fechaApertura = fechaApertura;
+	}
 
 }
