5a8d382a77968c70d6e1c1ed055d8bf61bb1790f
mardearc <jesumar01@gmail.com>
2024-11-15 19:22:01 +0100

    CtrlCuenta con Leer y Escribir Fichero

--- Practica03/src/controller/CtrlCuenta.java
+++ Practica03/src/controller/CtrlCuenta.java
@@ -1,12 +1,34 @@
 package controller;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Cuenta;
+
 public class CtrlCuenta {
 
-	public void EscribirFichero() {
-		
+	private final String FILENAME = "cuentas.dat";
+
+	public void escribirFichero(List<Cuenta> cuentas) {
+		try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILENAME))) {
+			oos.writeObject(cuentas);
+			System.out.println("Cuentas guardadas correctamente en " + FILENAME);
+		} catch (IOException e) {
+			System.err.println("Error al escribir en el fichero: " + e.getMessage());
+		}
 	}
-	
-	public void LeerFichero() {
-		
+
+	public List<Cuenta> leerFichero() {
+		List<Cuenta> cuentas = new ArrayList<>();
+		try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILENAME))) {
+			cuentas = (List<Cuenta>) ois.readObject();
+			System.out.println("Cuentas leídas correctamente desde " + FILENAME);
+		} catch (FileNotFoundException e) {
+			System.err.println("Archivo no encontrado: " + FILENAME);
+		} catch (IOException | ClassNotFoundException e) {
+			System.err.println("Error al leer del fichero: " + e.getMessage());
+		}
+		return cuentas;
 	}
 }
